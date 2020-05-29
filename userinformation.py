092ee9ce9bdb4f20ac0d3e8e943c1dec294c6b94
Limitized <65204970+Limitized@users.noreply.github.com>
2020-05-29 12:46:17 -0700

    Add files via upload

---
+++ userinformation.py
@@ -0,0 +1,13 @@
+###Edit Information Below###
+
+team = "NDT" #Insert team tag here
+
+spreadSheet_name = "NDT Team Stats" #Insert spreadsheet name here
+
+
+#Ignore the stuff below
+def getTeam():
+    return(team)
+
+def getSheetName():
+    return(spreadSheet_name)
